a4df01076480e4a3cbd8a48d5db9f2c69b59934b
mdshahzebalam <sus841226@gmail.com>
2024-09-10 22:38:00 +0530

    #Contant

--- ContactApi/ContactApi/obj/Debug/net8.0/ContactApi.AssemblyInfo.cs
+++ ContactApi/ContactApi/obj/Debug/net8.0/ContactApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84304bb2ca8779876b48fba89e7b4e3867fc4998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd7428de75d528d56fe3eceb45943f553927a80")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
