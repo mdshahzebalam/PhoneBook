5a1ce084e0e034144ff477059a2ab019e5963391
mdshahzebalam <sus841226@gmail.com>
2024-09-13 00:41:58 +0530

    Form To add New Contact

--- ContactUi/contact.web/src/app/app.component.ts
+++ ContactUi/contact.web/src/app/app.component.ts
@@ -4,18 +4,31 @@ import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Contacts } from '../models/contaxt.model';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   http = inject(HttpClient);
 
+  contactsForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phoneNumber: new FormControl<string>(''),
+    favorite: new FormControl<boolean>(false)
+  })
+
   contacts$ = this.getContacts();
+
+  onFormSubmit()
+  {
+    console.log(this.contactsForm.value);
+  }
 
   private getContacts(): Observable<Contacts[]>
   {
